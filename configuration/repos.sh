1ac6ddcdaf360000fec5365c538f8c5760250b07
Stephen Biggerstaff <s.biggerstaff@icloud.com>
2020-04-11 13:40:01 +0100

    Repo downloads

---
+++ configuration/repos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+git@github.com:sbiggerstaff/ptsessions.git ~/Projects/ptsessions
+git@github.com:sbiggerstaff/portfoliosite.git ~/Websites/portfolio
+git@github.com:sbiggerstaff/mileagelogger.git ~/Projects/mileagelogger
+git@github.com:sbiggerstaff/mileagecalc.git ~/Projects/mileagecalc
+git@github.com:sbiggerstaff/expensalitics.git ~/Projects/expensalitics
+git@github.com:sbiggerstaff/tokenscalc.git ~/Projects/tokenscalc
