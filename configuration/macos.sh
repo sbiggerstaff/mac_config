f6fd364fe9152deec4b9c4f759b1d71225514403
Stephen Biggerstaff <s.biggerstaff@icloud.com>
2020-12-18 18:27:25 +0000

    Adding github and firefox
    also removing a bug in the macos.sh script

--- configuration/macos.sh
+++ configuration/macos.sh
@@ -17,7 +17,7 @@ echo "======= Setting computer names ======="
 echo "Enter desired computer alias:"
 read computer_name
 sudo scutil --set ComputerName $computer_name
-sudo scutil --set HostName $computer_name/Users/stephenbiggerstaff/mac_config/configuration/macos.sh
+sudo scutil --set HostName $computer_name
 sudo scutil --set LocalHostName $computer_name
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $computer_name
 
